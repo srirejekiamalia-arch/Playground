27359a865974ef9f0a52604156259c38c895a88d
Sri Amalia <srirejekiamalia98@gmail.com>
2026-03-03 18:27:24 +0700

    amel

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-name= 100
+import os
 
-date= 12
-print (name+date)
-print (name-date)
-print (name/date)
+def cari_folder(root, nama):
+    hasil = []
+    
+    for root, dirs, files in os.walk(root):
+        if nama in dirs:
+            path_ditemukan = os.path.join(root, nama)
+            hasil.append(path_ditemukan)
+    
+    return hasil
+
+
+# ==== PEMAKAIAN ====
+root = "C://"  # ganti sesuai kebutuhan
+nama = "Downloads"  # nama folder yang dicari
+
+ditemukan = cari_folder(root, nama)
+
+if ditemukan:
+    print("Folder ditemukan:")
+    for d in ditemukan:
+        print(d)
+else:
+    print("Folder tidak ditemukan")
